57cc7b9634a6a98b68433d01a2937aa84bc98cd4
Noah Bunis <141171556+Noah-Bunis@users.noreply.github.com>
2026-02-16 18:06:40 -0500

    more clips

--- items/wik_clips.lua
+++ items/wik_clips.lua
@@ -1,14 +1,51 @@
-WIK_CLIPS = {{
+--[[ 
+CURRENT ODDS Yes:2 No:3
+
+FORMAT:
+{
+    filename = "",
+    title = "",
+    pausetime = float,
+    totaltime = float,
+    kills = boolean
+}
+    
+]]
+
+WIK_CLIPS = {
+{
     filename = "sf3",
     title = "Street Fighter 3: Third Strike (credit: Spiraler)",
-    pausetime = 8.5,
-    totaltime = 20,
+    pausetime = 2.9,
+    totaltime = 10,
     kills = false
-}, {
+},
+{
     filename = "gbvsr",
     title = "Granblue Fantasy Versus: Rising (credit: Light)",
     pausetime = 2.5,
     totaltime = 21,
     kills = true
+},
+{
+    filename = "isd",
+    title = "Idol Showdown (credit: PlusFramesGG)",
+    pausetime = 3.7,
+    totaltime = 36,
+    kills = true
+},
+{
+    filename = "t8",
+    title = "Tekken 8 (credit: OKG Chaos)",
+    pausetime = 5.5,
+    totaltime = 28,
+    kills = false
+},
+{
+    filename = "uni2",
+    title = "UNDER NIGHT IN-BIRTH II Sys:Celes (credit: DyingBlades)",
+    pausetime = 1.7,
+    totaltime = 20,
+    kills = false
 }
 }
